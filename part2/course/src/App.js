4a655160e368565e22f2358e2ec8b4b640a0be39
linran <linranff@gmail.com>
2022-09-19 17:18:16 +0800

    part2.a course module seperated

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,34 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Total = ({ parts }) => {
-  return (
-    <h4>
-      Total of {parts.reduce((sum, item) => sum + item.exercises, 0)} exercises
-    </h4>
-  );
-};
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return parts.map((item) => (
-    <Part key={item.id} name={item.name} exercises={item.exercises} />
-  ));
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const Courses = ({ courses }) => {
   return (
